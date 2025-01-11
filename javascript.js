099a4ecde169b2e974e8a100ade20d5f417c4fef
LUKSv11 <lukas.abrv@gmail.com>
2025-01-11 14:18:07 +0100

    uprava logiky aby fungovala s negativními čísly

--- javascript.js
+++ javascript.js
@@ -6,17 +6,14 @@ function poloha(){
     if(x>50 || y>50 || y<0 || x<0){
         document.getElementById("vysledek").innerHTML = "Leží mimo čtverec"
         document.getElementById("vysledek").style.color = "red"
-    } else{
-        document.getElementById("vysledek").innerHTML = "Neexistuje"
-    } 
-   if (x==50  || y==50 || y==0 || x==0){
-    document.getElementById("vysledek").innerHTML = "Leží na hranici"
-   }else{
-    if(x<50 && y<50){
+    } else if (x == 50 || y == 50 || y == 0 || x == 0){
+        document.getElementById("vysledek").innerHTML = "Leží na hranici"
+            document.getElementById("vysledek").style.color = "orange"
+    } else if (0 < x && x < 50 && 0 < y && y < 50) {
         document.getElementById("vysledek").innerHTML = "Leží uvnitř čtverce"
         document.getElementById("vysledek").style.color = "green"
-    } 
-    
+    } else {
+        document.getElementById("vysledek").innerHTML = "Leží mimo čtverec"
+        document.getElementById("vysledek").style.color = "red"
     }
-   
 }
